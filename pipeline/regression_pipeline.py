7c935ae5fd68ed6c664201bafb939d2112765fc5
MR-Elbarbary <121816435+MR-Elbarbary@users.noreply.github.com>
2025-05-19 19:17:02 +0300

    Add check_assumptions

--- pipeline/regression_pipeline.py
+++ pipeline/regression_pipeline.py
@@ -7,12 +7,14 @@ from models.polynomial import PolynomialRegression
 from models.ridge import RidgeRegression
 from tuning import gridSearch
 from metrics.evaluation import adjusted_r2_score
-import scipy.stats as sts
+from scipy.stats import spearmanr, pearsonr
 
 class RegressionPipeline:
-    def __init__(self, degree_range=(1, 5), alphas=[0.1, 1.0, 10.0]):
+    def __init__(self, degree_range=(1, 5), alphas=[0.1, 1.0, 10.0], independenceAlpha = 0.05, linearAlpha = 0.05):
         self.degrees = list(range(degree_range[0], degree_range[1] + 1))
         self.alphas = alphas
+        self.independenceAlpha = independenceAlpha
+        self.linearAlpha = linearAlpha
         self.preprocessor = None
         self.model = None
         self.results_ = {}
@@ -29,17 +31,25 @@ class RegressionPipeline:
         X_processed = self.preprocessor.fit_transform(X)
         return X_processed
 
-    def check_assumptions(self, X, y):
-        linear = self._check_linearity(X, y)
-        independent = self._check_independence(X)
+    def check_assumptions(self, X, y, independenceAlpha, linearAlpha):
+        linear = self._check_linearity(X, y, linearAlpha=linearAlpha)
+        independent = self._check_independence(X, independenceAlpha)
         return linear, independent
 
-    def _check_linearity(self, X, y):
-        # Dummy placeholder
+    def _check_linearity(self, X, y, linearAlpha):
+        for i in range(X.shape[1]):
+            _, p_value = pearsonr(X[:, i], y)
+            if p_value > linearAlpha:
+                return False  # Feature i is significantly linearly correlated with y
         return True
 
-    def _check_independence(self, X):
-        # Dummy placeholder
+    def _check_independence(self, X, independenceAlpha):
+        n_features = X.shape[1]
+        for i in range(n_features):
+            for j in range(i + 1, n_features):
+                _, p_value = spearmanr(X[:, i], X[:, j])
+                if p_value > independenceAlpha:
+                    return False
         return True
 
     def select_model(self, linear, independent, X_processed, y):
@@ -69,7 +79,7 @@ class RegressionPipeline:
 
     def fit(self, X, y):
         X_processed = self.preprocess(X)
-        linear, independent = self.check_assumptions(X_processed, y)
+        linear, independent = self.check_assumptions(X_processed, y, self.independenceAlpha, self.linearAlpha)
         self.select_model(linear, independent, X_processed, y)
 
     def predict(self, X):
